6932dce2ede10b3814a9b278b24e54bd8ca22bc5
ALFRED CHIBUIKE THEOPHANE <chibyk089@gmail.com>
2025-09-08 04:33:09 +0100

    refactor(index): replace exports with worker bootstrap entry point

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export * from './lib/async';
-export * from './lib/hash';
-export * from './lib/number';
+import { logger, startWorker } from "./lib/worker";
+
+startWorker().catch((err) => {
+    logger.error('Worker encountered a fatal error', { error: err });
+    process.exit(1); // Exit on unhandled errors
+});
