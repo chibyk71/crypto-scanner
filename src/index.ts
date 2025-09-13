7cd79a9b2a02ffa691247b9141661b8b14db2b60
ALFRED CHIBUIKE THEOPHANE <chibyk089@gmail.com>
2025-09-13 10:53:23 +0100

    refactor(logging): use dedicated logger in index and include stack trace on fatal errors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import { logger, startWorker } from "./lib/worker";
+import { createLogger } from "./lib/logger";
+import { startWorker } from "./lib/worker";
+
+const logger = createLogger('index');
 
 startWorker().catch((err) => {
-    logger.error('Worker encountered a fatal error', { error: err });
+    logger.error('Worker encountered a fatal error', { error: err, stack: err.stack });
     process.exit(1); // Exit on unhandled errors
 });
